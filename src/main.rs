94fdd4d6b4feaa86de601c79111ac6f41889e451
장승호(Seung Ho Jang) <skyser2003@ncsoft.com>
2020-02-04 14:15:53 +0900

    Url parsing successful

--- src/main.rs
+++ src/main.rs
@@ -14,7 +14,7 @@ fn main() {
     let mut cli_wrapper = SlackClientWrapper{ cli: &cli };
     let mut handler = SlackHandler{channel_id: String::new(), cli: &mut cli_wrapper, handlers: Vec::new()};
 
-    let namu_handler = NamuwikiHandler;
+    let namu_handler = NamuwikiHandler::default();
     handler.handlers.push(Box::new(namu_handler));
 
     let r = cli.run(&mut handler);
--- src/message_handler/namuwiki_handler.rs
+++ src/message_handler/namuwiki_handler.rs
@@ -1,14 +1,46 @@
 extern crate slack;
+extern crate regex;
 
 use slack::api::MessageStandard;
+use regex::Regex;
+use url::{Url, Host, Position};
 
 use crate::message_handler::MessageHandler;
 use crate::slack_handler::{SlackClientWrapper, SlackClientWrapperFunc};
 
-pub struct NamuwikiHandler;
+pub struct NamuwikiHandler {
+    title_regex: Regex
+}
+
+impl Default for NamuwikiHandler {
+    fn default() -> Self {
+        NamuwikiHandler{
+             title_regex: Regex::new(r"").unwrap()
+        }
+    }
+}
 
 impl MessageHandler for NamuwikiHandler {
     fn on_message(&mut self, cli: &mut SlackClientWrapper, msg: &MessageStandard) {
-        cli.send(msg.channel.as_ref().unwrap(), "안녕?");
+        let raw_text = msg.text.as_ref().unwrap();
+
+        if raw_text.chars().next().unwrap() != '<' || raw_text.chars().last().unwrap() != '>' {
+            return
+        }
+
+        let text = &raw_text[1..raw_text.len() - 1];
+        
+        let parse_result = Url::parse(text);
+
+        match parse_result {
+            Ok(parsed_url) => {
+                if parsed_url.host_str().unwrap() == "namu.wiki" {
+                    
+                }
+            }
+            Err(_) => {} // Not a URL
+        }
+
+        // cli.send(msg.channel.as_ref().unwrap(), "안녕?");
     }
 }

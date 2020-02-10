ab4f4a8773dc54e2b9e225b4cba8816f8c1bb8bc
장승호(Seung Ho Jang) <skyser2003@ncsoft.com>
2020-02-10 17:11:15 +0900

    Actix sample code added

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,17 @@
 use std::env;
+use actix_web::{get, web, App, HttpServer, Responder};
 
-fn main() {
+#[get("/{id}/{name}/index.html")]
+async fn index(info: web::Path<(u32, String)>) -> impl Responder {
+    format!("Hello {}! id: {}", info.1, info.0)
+}
+
+#[actix_rt::main]
+async fn main() -> std::io::Result<()> {
     let token = env::var("token").unwrap();
+
+    HttpServer::new(|| App::new().service(index))
+    .bind("0.0.0.0:80")?
+    .run()
+    .await
 }

0c293a77d0aa0343f14a9c003e31a26dbfbde1b3
Seung Ho Jang <shjang1992@gmail.com>
2020-04-22 02:20:07 +0900

    Test file separated

--- src/slack/mod.rs
+++ src/slack/mod.rs
@@ -92,42 +92,12 @@ pub struct BasicMessage<'a> {
     pub edited: Option<Edited<'a>>,
 }
 
-#[test]
-pub fn test_deserialize_basic_message() {
-    serde_json::from_str::<Message>(
-        r#"{
-        "type": "message",
-        "channel": "C2147483705",
-        "user": "U2147483697",
-        "text": "Hello world",
-        "ts": "1355517523.000005"
-    }"#,
-    )
-    .unwrap();
-}
-
 #[derive(Debug, Deserialize)]
 pub struct BotMessage<'a> {
     #[serde(flatten)]
     pub common: MessageCommon<'a>,
     pub bot_id: &'a str,
     pub icons: Icons<'a>,
-}
-
-#[test]
-pub fn test_deserialize_bot_message() {
-    serde_json::from_str::<Message>(
-        r#"{
-        "type": "message",
-        "subtype": "bot_message",
-        "ts": "1358877455.000010",
-        "text": "Pushing is the answer",
-        "bot_id": "BB12033",
-        "username": "github",
-        "icons": {}
-    }"#,
-    )
-    .unwrap();
 }
 
 #[derive(Debug, Deserialize)]

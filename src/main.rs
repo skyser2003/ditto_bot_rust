ad3990d3347daf659c8be69693c91a78c7453e01
장승호(Seung Ho Jang) <shjang1992@gmail.com>
2020-07-20 19:20:31 +0900

    Removed block

--- src/main.rs
+++ src/main.rs
@@ -141,37 +141,39 @@ impl Handler<MessageEvent> for SlackEventActor {
                     let parsed_url = Url::parse(link).unwrap();
                     let url_string = parsed_url.host_str().unwrap();
 
-                    if url_string == "namu.wiki" {
-                        let res = reqwest::get(link).await.unwrap();
-                        let body = res.text().await.unwrap();
-
-                        let title_opt = title_regex.captures(&body).and_then(|captures| {
-                            captures
-                                .get(1)
-                                .and_then(|match_title| Some(match_title.as_str()))
-                        });
-
-                        let title = match title_opt {
-                            Some(val) => format!("{} - 나무위키", val),
-                            None => "Invalid url".to_string(),
-                        };
-
-                        blocks.push(slack::BlockElement::Actions(slack::ActionBlock {
-                            block_id: None,
-                            elements: Some(vec![slack::BlockElement::Button(slack::ButtonBlock {
-                                text: slack::TextObject {
-                                    ty: "plain_text",
-                                    text: Cow::from(title),
-                                    emoji: None,
-                                    verbatim: None,
-                                },
-                                action_id: None,
-                                url: Some(link),
-                                value: None,
-                                style: Some("primary"),
-                            })]),
-                        }));
+                    if url_string != "namu.wiki" {
+                        return;
                     }
+
+                    let res = reqwest::get(link).await.unwrap();
+                    let body = res.text().await.unwrap();
+
+                    let title_opt = title_regex.captures(&body).and_then(|captures| {
+                        captures
+                            .get(1)
+                            .and_then(|match_title| Some(match_title.as_str()))
+                    });
+
+                    let title = match title_opt {
+                        Some(val) => format!("{} - 나무위키", val),
+                        None => "Invalid url".to_string(),
+                    };
+
+                    blocks.push(slack::BlockElement::Actions(slack::ActionBlock {
+                        block_id: None,
+                        elements: Some(vec![slack::BlockElement::Button(slack::ButtonBlock {
+                            text: slack::TextObject {
+                                ty: "plain_text",
+                                text: Cow::from(title),
+                                emoji: None,
+                                verbatim: None,
+                            },
+                            action_id: None,
+                            url: Some(link),
+                            value: None,
+                            style: Some("primary"),
+                        })]),
+                    }));
                 }
                 _ => {}
             }

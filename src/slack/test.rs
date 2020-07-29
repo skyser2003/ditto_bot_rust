396e1350bdec4ec86cdc523daefe237946c5f754
Gyusun Yeom <omniavinco@gmail.com>
2020-07-29 22:37:39 +0900

    Use Cow for unicode possible strings & add test case

--- src/slack/test.rs
+++ src/slack/test.rs
@@ -15,6 +15,25 @@ pub fn test_deserialize_basic_message() {
 }
 
 #[test]
+pub fn test_deserialize_unicode_basic_message() {
+    let deserialized = serde_json::from_str::<Message>(
+        r#"{
+        "type": "message",
+        "channel": "C2147483705",
+        "user": "U2147483697",
+        "text": "\uadf8\uc544\uc544",
+        "ts": "1355517523.000005"
+    }"#,
+    )
+    .unwrap();
+    if let Message::BasicMessage(msg) = deserialized {
+        assert_eq!(msg.common.text.as_ref(), "그아아");
+    } else {
+        panic!("deserialized one must be a BasicMessage!");
+    }
+}
+
+#[test]
 pub fn test_deserialize_bot_message() {
     serde_json::from_str::<Message>(
         r#"{

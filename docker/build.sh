0b25c3a7363a2352b10421d23e18c5f7494a9b16
Gyusun Yeom <omniavinco@gmail.com>
2022-01-16 20:32:28 +0900

    Docker with scratch

--- docker/build.sh
+++ docker/build.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 cd $(dirname ${BASH_SOURCE[0]})
 
-docker-compose build --pull --parallel && docker-compose push
+docker-compose build --pull --parallel --build-arg TARGETARCH=arm64 && docker-compose push

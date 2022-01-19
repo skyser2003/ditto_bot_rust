a42dbb6ec661813cf022e759470e0a51b1dc39f4
장승호(Seung Ho Jang) <shjang1992@gmail.com>
2022-01-19 14:40:19 +0900

    Markdown formatting fixed

--- src/modules/surplus.rs
+++ src/modules/surplus.rs
@@ -78,7 +78,7 @@ pub async fn handle<'a, B: crate::Bot>(bot: &B, msg: &crate::MessageEvent) -> an
 
             for pair in vec_table {
                 let user_name = user_name_map.get(pair.0).unwrap_or(pair.0);
-                let user_bar = format!("*{:}:\n\t*{:}", user_name, generate_bar(pair.1, 2));
+                let user_bar = format!("*{:}:*\n\t{:}", user_name, generate_bar(pair.1, 2));
 
                 vec_bar.push(user_bar);
             }

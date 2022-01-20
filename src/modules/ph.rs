57ab8ce87d934924c540c93006d21cf99b155f95
Gyusun Yeom <omniavinco@gmail.com>
2022-01-21 00:56:44 +0900

    handler unit-test

--- src/modules/ph.rs
+++ src/modules/ph.rs
@@ -15,3 +15,45 @@ pub async fn handle<'a, B: crate::Bot>(bot: &B, msg: &crate::MessageEvent) -> an
 
     Ok(())
 }
+
+#[tokio::test]
+#[cfg(test)]
+async fn test_ph() -> anyhow::Result<()> {
+    use crate::MessageEvent;
+
+    let bot: crate::test::MockBot = Default::default();
+
+    handle(
+        &bot,
+        &MessageEvent {
+            user: "".to_string(),
+            channel: "".to_string(),
+            text: "".to_string(),
+            link: None,
+        },
+    )
+    .await?;
+
+    assert!(bot.dump_messages()?.is_empty());
+
+    handle(
+        &bot,
+        &MessageEvent {
+            user: "".to_string(),
+            channel: "".to_string(),
+            text: "ㅍㅎ".to_string(),
+            link: None,
+        },
+    )
+    .await?;
+
+    let messages = bot.dump_messages()?;
+    assert_eq!(messages.len(), 1);
+    if let BlockElement::Text { text, .. } = &messages[0].1[0] {
+        assert_eq!(text, ":angdev:ㅊㅎ");
+    } else {
+        panic!("Wrong response");
+    }
+
+    Ok(())
+}

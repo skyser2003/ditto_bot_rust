2ae1dd5434380e6db4aa9b76ecaebf05eabc40d8
Gyusun Yeom <omniavinco@gmail.com>
2022-01-22 17:34:26 +0900

    Use alpine for runtime base image docker build make invalid image for scratch

--- docker/build.sh
+++ docker/build.sh
@@ -1,4 +1,9 @@
 #!/bin/bash
+set -e -o pipefail
 cd $(dirname ${BASH_SOURCE[0]})
 
-DOCKER_BUILDKIT=1 docker-compose build --pull --parallel --build-arg TARGETARCH=arm64 && docker-compose push
+DOCKER_BUILDKIT=1 docker-compose build --pull --parallel --build-arg TARGETARCH=arm64
+echo "======== build image inspect"
+docker image inspect registry.skyser.kr/shipduck/ditto_bot_rust
+echo "======== push image"
+docker-compose push

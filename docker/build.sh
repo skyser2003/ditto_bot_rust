7dd20f039afe3109e9ac3d64df8626330b704986
Seung Ho Jang <shjang1992@gmail.com>
2024-01-07 23:08:56 +0900

    Change arch to amd64

--- docker/build.sh
+++ docker/build.sh
@@ -2,7 +2,7 @@
 set -e -o pipefail
 cd $(dirname ${BASH_SOURCE[0]})
 
-DOCKER_BUILDKIT=1 docker-compose build --pull --parallel --build-arg TARGETARCH=arm64
+DOCKER_BUILDKIT=1 docker compose build --pull --parallel --build-arg TARGETARCH=amd64
 echo "======== build image inspect"
 docker image inspect registry.skyser.kr/shipduck/ditto_bot_rust
 echo "======== push image"

9f5e1b9da25424ae9f42b001674728a7b45ebfb1
Seung Ho Jang <shjang1992@gmail.com>
2024-01-08 21:56:04 +0900

    Namuwiki parsing fallback logic added

--- src/modules/namuwiki.rs
+++ src/modules/namuwiki.rs
@@ -27,10 +27,7 @@ pub async fn handle<B: crate::Bot>(bot: &B, msg: &crate::MessageEvent) -> anyhow
                 .captures(&body)
                 .and_then(|captures| captures.get(1).map(|match_title| match_title.as_str()));
 
-            match title_opt {
-                Some(val) => format!("{} - 나무위키", val),
-                None => "Invalid url".to_string(),
-            }
+            get_parsed_title(title_opt, &parsed_url)
         };
 
         bot.send_message(
@@ -55,5 +52,62 @@ pub async fn handle<B: crate::Bot>(bot: &B, msg: &crate::MessageEvent) -> anyhow
         )
         .await?;
     }
+    Ok(())
+}
+
+fn get_parsed_title(title_opt: Option<&str>, parsed_url: &Url) -> String {
+    let title_text = match title_opt {
+        Some(val) => val.to_string(),
+        None => {
+            let fragments = parsed_url.path_segments();
+
+            let mut fallback_title = Option::<String>::None;
+
+            if let Some(mut fragments) = fragments {
+                let wpart = fragments.next().unwrap_or_default();
+
+                if wpart == "w" {
+                    let rest_parts = fragments.collect::<Vec<_>>();
+
+                    let decoded = rest_parts
+                        .iter()
+                        .map(|part| {
+                            percent_encoding::percent_decode_str(part)
+                                .decode_utf8_lossy()
+                                .to_string()
+                        })
+                        .collect::<Vec<_>>()
+                        .join("/");
+
+                    fallback_title = Some(decoded);
+                }
+            }
+
+            match fallback_title {
+                Some(val) => val,
+                None => "Invalid url".to_string(),
+            }
+        }
+    };
+
+    format!("{} - 나무위키", title_text)
+}
+
+#[tokio::test]
+#[cfg(test)]
+async fn test_url_fallback() -> anyhow::Result<()> {
+    let link = "https://namu.wiki/w/Pok%C3%A9mon%20Sleep/%EC%9A%94%EB%A6%AC";
+    let invalid_link = "https://namu.wiki";
+    let parsed_url = Url::parse(link)?;
+    let failed_url = Url::parse(invalid_link)?;
+
+    let parse_success = get_parsed_title(Some("Pikachu"), &parsed_url);
+    let parse_failed = get_parsed_title(None, &parsed_url);
+    let all_failed = get_parsed_title(None, &failed_url);
+
+    assert_eq!(parse_success, "Pikachu - 나무위키");
+    assert_eq!(parse_failed, "Pokémon Sleep/요리 - 나무위키");
+    assert_eq!(all_failed, "Invalid url - 나무위키");
+
     Ok(())
 }
